2ea8f8bd7827435dd44b3dd84405a8f462eea0c9
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2021-12-04 13:02:32 +0200

    h

---
+++ button.js
@@ -0,0 +1,89 @@
+const fs = require("fs")
+if(!fs.existsSync("./data.json")){
+	let defaultData = {
+		health: 360,
+		isAlive: true,
+		healthLevel: 6
+	}
+	fs.writeFileSync("./data.json", JSON.stringify(defaultData, null, 4))
+}
+const data = require("./data.json")
+
+const ranks = [
+	{
+		name: "A",
+		maxHealth: 360,
+		minHealth: 291,
+	},
+	{
+		name: "B",
+		maxHealth: 290,
+		minHealth: 221
+	},
+	{
+		name: "C",
+		maxHealth: 220,
+		minHealth: 161
+	},
+	{
+		name: "D",
+		maxHealth: 160,
+		minHealth: 101
+	},
+	{
+		name: "E",
+		maxHealth: 100,
+		minHealth: 41
+	},
+	{
+		name: "F",
+		maxHealth: 40,
+		minHealth: 1
+	}
+]
+
+// Hello, It's me again. I am not sure this is the smartest way but it doesnt actually really matter.
+function init() {
+	var healthDecrease = setInterval(() => {
+		if(data.health <= 0){
+			data.isAlive = false
+			data.health = 0
+			data.healthLevel = "dead"
+			clearInterval(healthDecrease)
+		} else {
+			data.health -= 1
+			data.healthLevel = getRank(data.health).name
+		}
+		console.log("Health decreased! Now: ", data.health)
+		fs.writeFileSync("./data.json", JSON.stringify(data, null, 4))
+	}, 6000)
+}
+
+function getRank(health) {
+	for(let rank in ranks){
+		if(health >= ranks[rank].minHealth && health <= ranks[rank].maxHealth){
+			return ranks[rank]
+		}
+	}	
+}
+
+function slapthebutton() {
+	if(data.isAlive){
+		let rank = getRank(data.health)
+		data.health = 360
+		data.healthLevel = getRank(data.health).name
+		fs.writeFileSync("./data.json", JSON.stringify(data, null, 4))
+		return {rank: rank}
+	} else {
+		return {message: "Pressing a dead button wont help..."}
+	}
+}
+
+module.exports = {
+	getRank,
+	init,
+	slapthebutton,
+	isAlive: data.isAlive,
+	health: data.health,
+	healthLevel: data.healthLevel
+}
--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const rateLimit = require("express-rate-limit");
 const express = require('express')
+var session = require('express-session')
 var Recaptcha = require('express-recaptcha').RecaptchaV3;
 require('dotenv').config()
+const button = require('./button')
 
 var recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY, {callback: "cb", "action": "buttonDEV"});
 const app = express()
 const port = 81
+button.init()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: true
+}))
 
 app.set('view engine', 'ejs');
 
+app.get("/health", function(req, res){
+	res.send({healthLevel: button.healthLevel, isAlive: button.isAlive, health: button.health})
+})
+
 app.get('*', recaptcha.middleware.render, function(req, res){
-    res.render(__dirname + '/index.ejs', { captcha: res.recaptcha })
+	res.render(__dirname + '/index.ejs', { captcha: res.recaptcha, health: button.healthLevel, isAlive: button.isAlive })
 });
 
-app.post('*', recaptcha.middleware.verify, function(req, res){
-	if (req.recaptcha.data && req.recaptcha.data.score < 0.7) return res.status(403).send('Captcha failed')
-	res.render(__dirname + '/index.ejs', {
-        error: req.recaptcha.error,
-		captcha: false,
-        data: JSON.stringify(req.recaptcha.data),
-    })
+
+app.post('*', recaptcha.middleware.verify, function(req, res){ 
+	if(req.recaptcha.error == "timeout-or-duplicate") return res.redirect('/?captcha=timeout')
+	if (req.recaptcha.data && req.recaptcha.data.score < 0.7) return res.status(403).redirect('/?captcha=failed')
+	let resp = button.slapthebutton()
+	if(resp.message) return res.send(resp.message)
+	req.session.rank = resp.rank.name
+	res.redirect('/')
 });
 
 app.listen(port, () => {
