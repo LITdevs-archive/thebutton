a239851416d8c7bbfda8082c36845ea47410c75c
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2021-12-11 19:50:29 +0200

    h

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const port = 83
 button.init()
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 1000, // 15 minutes
 	max: 1 // limit each IP to 100 requests per windowMs
 });
 app.use(express.json());
